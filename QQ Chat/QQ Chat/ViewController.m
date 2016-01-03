acedd1fd0657ae7a0338d8af770853d98436f27a
Hao Chen <hchen56@u.rochester.edu>
2016-01-02 19:16:32 -0500

    complete messageCell

--- QQ Chat/QQ Chat/ViewController.m
+++ QQ Chat/QQ Chat/ViewController.m
@@ -9,26 +9,34 @@
 #import "ViewController.h"
 #import "CZMessage.h"
 #import "CZMessageCell.h"
+#import "CZMessageFrame.h"
 
 @interface ViewController () <UITableViewDataSource>
-@property (nonatomic, strong) NSArray *messages;
+@property (nonatomic, strong) NSArray *messageFrames;
 @property (weak, nonatomic) IBOutlet UITableView *UITableView;
 @end
 
 
 @implementation ViewController
 //懒加载
-- (NSArray *)messages
+- (NSArray *)messageFrames
 {
-    if (_messages == nil) {
-        _messages = [CZMessage messagesList];
+    if (_messageFrames == nil) {
+        NSArray *messages = [CZMessage messagesList];
+        NSMutableArray *tmpArray = [NSMutableArray array];
+        for (CZMessage *msg in messages){
+            CZMessageFrame *msgFrame = [[CZMessageFrame alloc] init];
+            msgFrame.message = msg;
+            
+            [tmpArray addObject:msgFrame];
+        }
+        _messageFrames = tmpArray;
     }
-    return _messages;
+    return _messageFrames;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSLog(@"%@", self.messages);
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -40,7 +48,7 @@
 #pragma mark - 数据源的方法
 - (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return self.messages.count;
+    return self.messageFrames.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -48,7 +56,7 @@
     //1 创建自定义的可重用cell
     CZMessageCell *cell = [CZMessageCell messageCellWithTableView:tableView];
     //2 给子控件赋值
-    cell.message = self.messages[indexPath.row];
+    cell.messageFrame = self.messageFrames[indexPath.row];
 }
 
 @end
